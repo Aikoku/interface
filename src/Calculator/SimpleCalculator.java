5d6c230ca39951c41b119ac864046875696629ef
Mark <hotmark91@gmail.com>
2014-05-05 20:24:11 +0300

    calculator added

--- src/Calculator/SimpleCalculator.java
+++ src/Calculator/SimpleCalculator.java
@@ -1,3 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package Calculator;
 
 import java.awt.FlowLayout;
@@ -43,6 +49,3 @@ public class SimpleCalculator {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-    
-    
-
