ced232d0f2383f541966d9b75fd2d2faed0573fb
Mark <hotmark91@gmail.com>
2014-06-02 18:43:15 +0300

    Stenografy Start

---
+++ src/Steganography/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Steganography;
+
+/**
+ *
+ * @author Mark
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        myStegFrame form = new myStegFrame();
+        form.setLocationRelativeTo(null);
+        form.setVisible(true);
+    }
+    
+}
